a1cf6ce7d0ad80bc5ae70b885688fc79adc292ad
isabelyb <isabelcristinayepesb@gmail.com>
2021-06-23 21:33:56 -0500

    Starting with Js

---
+++ js_basic/values.js
@@ -0,0 +1,29 @@
+//PRIMITIVE VALUES
+
+// Numbers
+40
+
+// Text
+"Isabely"
+
+
+//Boolean
+true
+false
+
+//Empty Value
+null
+undefined
+
+
+//OBJECT 
+
+//Array
+[1,2,3]
+
+
+//Object
+{ name: "Isabely"}
+
+
+
