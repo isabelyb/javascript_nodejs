c4f5e4e0e3c658dd7aa98e64a03299f87ae75216
isabelyb <isabelcristinayepesb@gmail.com>
2021-07-12 14:33:36 -0500

    operators and conditionals

---
+++ js_basic/09_if_else.js
@@ -0,0 +1,29 @@
+if (true) {
+    console.log("Hello");
+}
+//Hello
+
+if (false) {
+    console.log("Hello");
+}
+//undefined
+
+
+if (true) {
+    console.log("Hello");
+} else {
+    console.log("It's False");
+}
+// Hello
+
+
+if (false) {
+    console.log("Hello");
+} else {
+    console.log("It's False");
+}
+// It's False
+
+
+
+
---
+++ js_basic/10_else_if.js
@@ -0,0 +1,17 @@
+var age = 18;
+
+if (age == 18) {
+    console.log("You can vote, It will be your first time");
+} else if (age > 18) {
+    console.log("You can vote again");
+} else {
+    console.log("You can't vote")
+}
+
+
+
+// condition ? true : false;
+
+var number = 1;
+var result = number === 1 ? "I am one" : "I am not one";
+console.log(result);
