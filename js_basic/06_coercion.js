50443d7e8d71fe3ea67aa6e79e22f2f425cce4f2
isabelyb <isabelcristinayepesb@gmail.com>
2021-07-12 14:01:01 -0500

    coercion and truthy and falsy

---
+++ js_basic/06_coercion.js
@@ -0,0 +1,23 @@
+var a = 4 + "7"
+typeof a
+// "string"
+
+var b = 4 * "7"
+typeof b
+// "number"
+
+
+
+var c = 20
+typeof c
+//"number"
+
+
+var d = String(c);
+typeof d
+//"string"
+
+
+var e = Number(d);
+typeof e
+//"number"
