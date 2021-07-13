9cf422357005fe7c41328aa64576cfb1b2dfa677
isabelyb <isabelcristinayepesb@gmail.com>
2021-07-13 14:07:40 -0500

    switch

---
+++ js_basic/12_switch.js
@@ -0,0 +1,18 @@
+var number =  Math.floor(Math.random() * 3)
+
+switch(number){
+    case 0:
+        console.log("I am 0");
+        break;
+    case 1:
+        console.log("I am 1");
+        break;
+    case 2:
+        console.log("I am 2");
+        break;
+    case 3:
+        console.log("I am 3");
+        break;
+    default:
+        console.log("I'm not a number :(")                
+}
